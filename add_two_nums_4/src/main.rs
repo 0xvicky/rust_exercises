31d725eeb0c95a3ca70d2ad40b6838e734227340
vicky <tyagivivek528@gmail.com>
2026-01-08 10:07:16 +0000

    code optimised

--- add_two_nums_4/src/main.rs
+++ add_two_nums_4/src/main.rs
@@ -1,43 +1,46 @@
-use std::io;
+use std::io;    
+
+fn parse_str_to_int(inp_buf: &str) -> (Vec<i32>, Vec<String>){
+    let mut valids:Vec<i32> = Vec::new();
+    let mut invalids:Vec<String> = Vec::new();
+
+      let inp_itr = inp_buf.split_whitespace();
+
+      inp_itr.for_each(|f| {
+        match f.parse::<i32>()
+      {  Ok(d)=>{
+          valids.push(d);
+      }
+        Err(_)=>{
+            invalids.push(f.to_string());
+        }
+    }
+      });
+    (valids, invalids)
+}
 fn main() {
     println!("Add nums from input_4");
     //prompt user to enter numbers separated by space
     println!("Enter the numbers separated by space:😏");
     let mut inp_buffer = String::new();
-    let mut invalid_inputs:Vec<String> = Vec::new();
     //take input buffer from io and convert it into an iterator
     io::stdin().read_line(&mut inp_buffer).expect("Error occured while taking input");
     //map over the iterator and first parse and then add it to result
     // let inp_itr:i32 = inp_buffer.split_whitespace().filter_map(|x| x.parse::<i32>().ok()).sum();
-
-    let inp_itr:i32 = inp_buffer.split_whitespace().filter_map(|x| {
-        match x.parse::<i32>(){
-            Ok(num)=>{Some(num)},
-            Err(_) =>{
-                // println!("Error occured while parsing: {e}");
-                // println!("{x}");
-                invalid_inputs.push(x.to_string());
-                None
-            }
-        }
-    } 
-).sum();
+    let (valids, invalids) = parse_str_to_int(&inp_buffer);
+    let res:i32 = valids.into_iter().sum();
 
     //print the result
-    println!("Result : {inp_itr}");
+    println!("Result : {res}");
 println!("================================================================");
 println!("Stats");
 println!("================================================================");
     //print all the invalid inputs
-    println!("Invalid Inputs:{}", invalid_inputs.len());
-    invalid_inputs.iter().for_each(|x| println!("{}", x.trim()));
+    println!("Invalid Inputs:{}", invalids.len());
+    invalids.iter().for_each(|x| println!("{}", x.trim()));
 
 }
 
 /*
-• Reject invalid input and print which value failed
-• Accept floats instead of ints
-• Reduce using .fold() instead of sum()
-• Sort and print the numbers before summing
-• Compute mean/median
+• Reject invalid input and print which value failed ✅
 */
